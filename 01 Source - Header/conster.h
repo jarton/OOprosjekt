588575767e6dc98f4b4cc75c7fb35c5f932e14ab
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-24 10:40:11 +0100

    Endret filnavn + lagt til 2stk const int

--- 01 Source - Header/conster.h
+++ 01 Source - Header/conster.h
@@ -1,4 +1,6 @@
 // CONSTER
 
-const int MAXSONER = 100;       // Maks antall soner
-const int STRLEN = 60;          // Standard string lengde
+const int MAXSONER = 100;           // Maks antall soner
+const int NVNLEN = 60;              // Maks navnlengde
+const int STRLEN = 60;              // Standard string lengde
+const int MAXDATAFILER = 9999999;   // Maks antall .inf og .dta filer
