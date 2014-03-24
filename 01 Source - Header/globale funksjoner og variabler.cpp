45030d3eece21095c01d86faa35c298caebdbfb4
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-24 12:08:23 +0100

    3 funksjoner i globale variabler (h og cpp)

---
+++ 01 Source - Header/globale funksjoner og variabler.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+//  Leser inn en tekst med lengde ulik 0:
+void lesTxt(char t[], char s[], const int LEN) {    
+  do  {
+    cout << '\t' << t << ": ";                            //  Skriver ledetekst
+    cin.getline(s, LEN);                                    //  Leser inn tekst
+  } while (strlen(s) == 0);               //  Sjekker at tekstlengden er ulik 0
+}
+
+
+//  Leser og returnerer ETT upcaset tegn.
+char les()  {                    
+  char ch;
+  cout << "\n\nKommando:  ";
+  cin >> ch;
+  return (toupper(ch));
+}
+
+
+//  Skriver ledetekst (t), leser og returnerer et tall mellom min og max:
+int lesTall(char* t, int min, int max)  {
+  int tall;
+  do  {
+    cout << '\t' << t << " (" << min << '-' << max <<  "):  ";
+    cin >> tall;  cin.ignore();
+  } while (tall < min  ||  tall > max);
+  return tall;
+}
--- 01 Source - Header/globale funksjoner og variabler.h
+++ 01 Source - Header/globale funksjoner og variabler.h
@@ -1,7 +1,7 @@
 #if !defined(__GL_FV_H)
 #define __GL_FV_H
 
-int  les_tall(char* t, int min, int max);          // Leser inn et tall 
+int  lesTall(char* t, int min, int max);          // Leser inn et tall 
 void  lesTxt(char t[], char s[], const int LEN);   // Leser inn en tekst
 char les();                                        // Leser inn et tegn
 
