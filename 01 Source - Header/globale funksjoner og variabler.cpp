cf1b174276b539d023fecc78c2292283f4172ec0
Jarton <jebbis92@gmail.com>
2014-03-24 14:01:53 +0100

    lagnavn funksjon v1

--- 01 Source - Header/globale funksjoner og variabler.cpp
+++ 01 Source - Header/globale funksjoner og variabler.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include <cstdlib>																// itoa
 #include "conster.h"
 #include "class Soner.h";
 #include "class Kunder.h";
+#include <cstring>
 using namespace std;
 
 
@@ -19,6 +21,24 @@ void lesTxt(char t[], char s[], const int LEN) {
   } while (strlen(s) == 0);               //  Sjekker at tekstlengden er ulik 0
 }
 
+void LagNavn(char* t, char* s1, char * s2, int n, int LEN) {   //Lager filer
+	int tall = strlen(s1) + strlen (s2) + LEN;	            //Antall tegn i navn
+	int i = 0;                                                  //int for løkke
+	t = new char[tall+1];                       //Lager en array for filnavn
+	char tall[] = itoa(n);                         //Gjør nummeret om til tekst
+	int potens = 10 ^ (LEN-1);                     //Tall for regning av desimaler
+	while ((potens > LEN) || potens == 1) {       //Løkke som finner antall nuller
+		potens /= 10;                             //Deler på 10 til antall er funnet
+		i++;	
+	}
+char nuller[i];
+for (int j = 0; j < i; j++) {
+ 	nuller[j]='0';	
+	}
+strcat(s1, nuller);
+strcat(s1, s2)
+t = s1;
+}
 
 //  Leser og returnerer ETT upcaset tegn.
 char les()  {                    
