001b242d5ba86125584f108dc3301c46cafd1f4a
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-24 13:31:30 +0100

    #include i class Bolig

--- 01 Source - Header/class Bolig.h
+++ 01 Source - Header/class Bolig.h
@@ -1,6 +1,9 @@
 #if !defined(__class_Bolig_H)
 #define __class_Bolig_H
 
+#include "class Eiendom.h";
+#include "enumer.h";
+
 class Bolig : public Eiendom {
 private:
 	int byggeaar, bruttoareal, boareal, antSoverom;
