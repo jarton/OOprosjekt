bc985cda5ee6b155ebe04dad1ad89b4ccbe9d1b1
Per-Kristian <130397@hig.no>
2014-03-24 12:59:52 +0100

    La til funksjoner i globale.h

--- 01 Source - Header/globale funksjoner og variabler.h
+++ 01 Source - Header/globale funksjoner og variabler.h
@@ -6,5 +6,15 @@ void  lesTxt(char t[], char s[], const int LEN);   // Leser inn en tekst
 char les();                                        // Leser inn et tegn
 
 void skrivMeny();		//Skriver ut menyen til display
+void eiendomDisplay();
+void eiendomNy();
+void eiendomSlett();
+void eiendomEndre();
+void soneDisplay();
+void kundeDisplay();
+void kundeNy();
+void kundeSlett();
+void kundeEndre();
+void ukentlig();
 
 #endif
