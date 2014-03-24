d063f7bfd14c8c941b27eef18d795430bef95e34
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-24 13:34:03 +0100

    #include

--- 01 Source - Header/class Kunder.h
+++ 01 Source - Header/class Kunder.h
@@ -1,6 +1,8 @@
 #if !defined(__class_Kunder_H)
 #define __class_Kunder_H
+
 #include "listtool2.h"
+#include "enumer.h";
 
 class Kunder : public Num_element {
 private:
--- 01 Source - Header/class Sone.h
+++ 01 Source - Header/class Sone.h
@@ -1,5 +1,6 @@
 #if !defined(__class_Sone_H)
 #define __class_Sone_H
+
 #include "listtool2.h"
 
 class Sone {
