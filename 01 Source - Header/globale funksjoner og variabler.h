254664d72e9817239e9007d9857d1ea7fc37140e
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-24 12:02:40 +0100

    3 gloable funksjoner i H

--- 01 Source - Header/globale funksjoner og variabler.h
+++ 01 Source - Header/globale funksjoner og variabler.h
@@ -1,7 +1,9 @@
 #if !defined(__GL_FV_H)
 #define __GL_FV_H
 
-int  les_tall(char* t, int min, int max);   // Leser inn et tall mellom min\max
+int  les_tall(char* t, int min, int max);          // Leser inn et tall 
+void  lesTxt(char t[], char s[], const int LEN);   // Leser inn en tekst
+char les();                                        // Leser inn et tegn
 
 
 #endif
