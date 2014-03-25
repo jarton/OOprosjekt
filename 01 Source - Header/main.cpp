ba0cd3047dfa7a87e6db498af4ccde6b4515bc4e
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-25 12:01:01 +0100

    1

--- 01 Source - Header/main.cpp
+++ 01 Source - Header/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include "globale funksjoner og variabler.h"
 #include "class Soner.h"
-#include "class Sone.h"
-#include "class Kunde.h"
-
 #include "class Kunder.h"
 
 using namespace std;
@@ -13,10 +10,11 @@ using namespace std;
 int main()  {
 
 	char valg;                               //  Brukerens valg/kommando.
-	Soner soner;
-	Kunder kunder;
+	Soner* soner;
+	Kunder* kunder;
 	//List* Kundeliste;
 	//List* Soneliste;
+	soner;
 
 	//Kundeliste = new List(Sorted);
 	//Soneliste = new List(Sorted);
@@ -28,9 +26,9 @@ int main()  {
   valg = les();
   while (valg != 'Q')  {
     switch(valg)  {
-      case 'E': soner.fortsettelseMeny(valg);  break;  // Eiendom
-			case 'S': soner.fortsettelseMeny(valg);  break;  // Sone (display)
-      case 'K': kunder.fortsettelseMeny(); break;  // Kunde
+      case 'E': soner->fortsettelseMeny(valg);  break;  // Eiendom
+			case 'S': soner->fortsettelseMeny(valg);  break;  // Sone (display)
+      case 'K': kunder->fortsettelseMeny(); break;  // Kunde
       //case 'U': ;         break;  //  Senere
       default : skrivMeny();	 break; // 
     }
