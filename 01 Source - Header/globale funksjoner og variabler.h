3aa35548a333732c83bc325a18921031d38b4126
Kristian Dragerengen <kristian@dragerengen.no>
2014-03-27 11:09:14 +0100

    rettet feil

--- 01 Source - Header/globale funksjoner og variabler.h
+++ 01 Source - Header/globale funksjoner og variabler.h
@@ -6,9 +6,6 @@ void  lesTxt(char t[], char s[], const int LEN);   // Leser inn en tekst
 char les();                                        // Leser inn et tegn
 
 void skrivMeny();		         //Skriver ut menyen til display
-void SEMeny(char valg);
-void KMeny();
-
 void eiendomDisplay();
 void eiendomNy();
 void eiendomSlett();
