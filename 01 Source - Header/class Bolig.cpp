d88cf9c6351d9409d1aa30fc43b659c75e17da1e
Per-Kristian <130397@hig.no>
2014-04-07 11:47:40 +0200

    Kommentering
    Bolig.cpp

--- 01 Source - Header/class Bolig.cpp
+++ 01 Source - Header/class Bolig.cpp
@@ -3,33 +3,33 @@
 #include "class Eiendom.h"
 #include "class Bolig.h"
 #include <fstream>
-#include "conster.h"
+#include "conster.h" //STRLEN
 
 using namespace std;
 
 //Parameterløs constructor som leser input til data
 Bolig::Bolig(int oppdragsnr) : Eiendom(oppdragsnr) 	{
-	char buffer[STRLEN];
+	char kommando;
 
-	byggeaar = lesTall("byggeår", 1800, 2014);
-	bruttoareal = lesTall("bruttoareal", 2, 1000);
-	boareal = lesTall("boareal", 2, 1000);
-	antSoverom = lesTall("antall soverom", 0, 250);
-	lesTxt("til   Leie / Salgs", buffer, STRLEN);
-	if (buffer[0] == 'L' || 'l')
+	byggeaar = lesTall("byggeår", 1800, 2014);			//Leser byggeår
+	bruttoareal = lesTall("bruttoareal", 2, 1000);	//Leser bruttoareal
+	boareal = lesTall("boareal", 2, 1000);					//Leser boareal
+	antSoverom = lesTall("antall soverom", 0, 250);	//Leser antall soverom
+	cout << "\ntil leie eller salgs (L/S): ";		//Leser om den er til leie eller salgs
+	if (kommando == 'L')
 		statusonsket = leie;
-	else if (buffer[0] == 'S' || 's')
+	else if (kommando == 'S')
 		statusonsket = salg;
 }
 
 //Constructor som leser alle boligdata fra fil
 Bolig::Bolig(ifstream & inn, int onr) : Eiendom(inn, onr)	{
-	inn >> byggeaar >> bruttoareal
+	inn >> byggeaar >> bruttoareal		//leser relevante data fra fil
 		>> boareal >> antSoverom;
 	inn.ignore();
 	char buffer[STRLEN];
-	inn.getline(buffer, STRLEN);
-	switch (buffer[0])	{
+	inn.getline(buffer, STRLEN); //Leser om til salg eller leie
+	switch (buffer[0])	{	//Sjekker første bokstav på linjen
 	case 's': statusonsket = salg; break;
 	case 'l': statusonsket = leie; break;
 	}	
@@ -42,25 +42,25 @@ Bolig::~Bolig()  {
 
 //Skriver alle boligdata til fil
 void Bolig::skrivBoligTilFil(ofstream & ut)	{
-	skrivTilFil(ut);
-	ut << '\n' << byggeaar << '\n'
+	skrivTilFil(ut); //Skriver Eiendomsdata.
+	ut << '\n' << byggeaar << '\n' //Skriver Boligdata.
 		<< bruttoareal << '\n'
 		<< boareal << '\n'
 		<< antSoverom << '\n';
-	if (statusonsket == leie)
+	if (statusonsket == leie) //Skriver enten "leie" eller "salg".
 		ut << "leie";
 	else
 		ut << "salg";
 }
 
-bool Bolig::type()	{
-	return 0;
+bool Bolig::type()	{ //Funksjon for å sjekke objektets type.
+	return 0; // (0 tilsvarer bolig, 1 tilsvarer eiendom.)
 }
 
-void Bolig::displayBolig()  {
+void Bolig::displayBolig()  { //Skriver info til display
 	cout << "\n--BOLIG--\n";
-	display();
-	cout << "\nByggeaar: "<< byggeaar;
+	display(); //Viser data fra Eiendom.
+	cout << "\nByggeaar: "<< byggeaar;	//Viser data fra Bolig..
 	cout << "\nBruttoadreal: " << bruttoareal;
 	cout << "\nBoareal: " << boareal;
 	cout << "\nAntall Soverom: " << antSoverom;
