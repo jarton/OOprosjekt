b8131d98b00f656cf0b2f0dc1e3d85a24f3b669a
Kristian Dragerengen <kristian@dragerengen.no>
2014-04-07 15:54:29 +0200

    Kommentering Kunde

--- 01 Source - Header/class Kunde.cpp
+++ 01 Source - Header/class Kunde.cpp
@@ -1,10 +1,10 @@
-#include <iostream>
+#include <iostream>                              // cin, cout
 #include "class Kunde.h"
 #include "class Kunder.h"
-#include <cstdlib>
-#include <cstring>
-#include <fstream>
-#include "conster.h"
+#include <cstdlib>                              // itoa
+#include <cstring>                              // strcpy
+#include <fstream>                              // ifstream ofstream
+#include "conster.h" 
 #include "globale funksjoner og variabler.h"
 #include "class IntrSone.h"
 
@@ -38,56 +38,54 @@ void Kunde::displayKunde()  {
 }	
 
 
-
-Kunde :: Kunde(int knr) : Num_element(knr) {
+// Kunde constructor som legger til ny kunde
+Kunde :: Kunde(int knr) : Num_element(knr) {  
 	char ch[STRLEN];
 	char kommando;
-	IntrSone* intrsonen; // Peker til intrsone-objekt
-	IntrSone* intrsonekopi; // Peker til kopiobjekt.
+	IntrSone* intrsonen;                         // Peker til intrsone-objekt
+	IntrSone* intrsonekopi;                      // Peker til kopiobjekt.
 
-	kundenummer = knr;
+	kundenummer = knr;           
 	cout << "\n\nTILDELT KUNDENUMMER: " << knr;
 
-	tlf = lesTall("\nTelefon ", 10000000, 99999999); //Leser tlf-nummer
+	tlf = lesTall("\nTelefon ", 10000000, 99999999);      //Leser tlf-nummer
 
-	lesTxt("\nNavn", ch, STRLEN); //leser navn
-	navn = new char[strlen(ch)+1];
+	lesTxt("\nNavn", ch, STRLEN);                         //leser inn navn
+	navn = new char[strlen(ch)+1];             // lager ny char med eksakt lengde
 	strcpy(navn, ch);
 
-	lesTxt("\nGateadresse", ch, STRLEN); //leser gateadresse
-	gateadresse = new char[strlen(ch)+1];
+	lesTxt("\nGateadresse", ch, STRLEN);             //leser gateadresse
+	gateadresse = new char[strlen(ch)+1];      // lager ny char med eksakt lengde
 	strcpy(gateadresse, ch);
 
-	lesTxt("\nPostnummer", ch, STRLEN); //leser postnummer
-	postadresse = new char[strlen(ch)+1];
+	lesTxt("\nPostnummer", ch, STRLEN);              //leser postnummer
+	postadresse = new char[strlen(ch)+1];      // lager ny char med eksakt lengde
 	strcpy(postadresse, ch);
 
-	lesTxt("\nE-post", ch, STRLEN); // leser mail
-	mail = new char[strlen(ch)+1];
+	lesTxt("\nE-post", ch, STRLEN);              // leser mail
+	mail = new char[strlen(ch)+1];           // lager ny char med eksakt lengde
 	strcpy(mail, ch);
 
-	IntrSoneliste = new List(Sorted); //lager IntrSone-liste.
-	int sonenr = lesTall("\nSonenummer", 1, 100);
+	IntrSoneliste = new List(Sorted);            //lager IntrSone-liste.
+	int sonenr = lesTall("\nSonenummer", 1, 100);  // leser inn tall 1-100
 	
-	intrsonen = new IntrSone(sonenr); //Nytt IntrSone objekt..
-	IntrSoneliste->add(intrsonen); //Legger det til i listen.
+	intrsonen = new IntrSone(sonenr);                 //Nytt IntrSone objekt..
+	IntrSoneliste->add(intrsonen);                     //Legger det til i listen.
 	
 	cout << "\nLegge til en ny sone? (J/N)";
-	kommando = les();
+	kommando = les();                                // Leser inn et upcaset tegn
 
-	while (kommando == 'J') { //Hvis "ja".
-		intrsonekopi = new IntrSone(*intrsonen); //Default copy constructor..
-		sonenr = lesTall("\nSonenummer", 1, 100); //Leser sonenummer.
-		intrsonekopi->endreSonenr(sonenr); //Endrer sonenummeret på kopien.
-		IntrSoneliste->add(intrsonekopi); //Legger til kopien i listen.
+	while (kommando == 'J') {                      //Hvis "ja".
+		intrsonekopi = new IntrSone(*intrsonen);       //Default copy constructor
+		sonenr = lesTall("\nSonenummer", 1, 100);        //Leser sonenummer.
+		intrsonekopi->endreSonenr(sonenr);        //Endrer sonenummeret på kopien.
+		IntrSoneliste->add(intrsonekopi);               //Legger til kopien i listen.
 		cout << "\n\nLegge til en ny sone? (J/N)";
-		kommando = les();
+		kommando = les();                             // Leser inn et upercaset teg
 	}
-	
-
 }
 
-
+// Funksjon som returnerer true\false om kundenummer er rett
 bool Kunde::sjekkNr(int Knr)  {
 	if (Knr == kundenummer)
 		return true;
@@ -95,61 +93,59 @@ bool Kunde::sjekkNr(int Knr)  {
 		return false;
 }
 
-
+// Constructor som leser fra fil
 Kunde::Kunde(ifstream & inn, int nr) : Num_element(nr)  {
-	//kundeliste = new List(Sorted);
-	char buffer[STRLEN];
-	int ant;
-	IntrSone* intrsone;
+	char buffer[STRLEN];                 // Buffer til innlesing
+	int ant;                         // antall interessesoner
+	IntrSone* intrsone;                //Interessesonepeker
 
-	inn >> kundenummer;
-	inn.ignore();
-	if (kundenummer == nr)  {
-	inn.getline(buffer,STRLEN);
-	navn = new char[strlen(buffer + 1)];
+	inn >> kundenummer;                 // Leser inn kundenummer
+	inn.ignore();                        // ignorere 1 linje
+	if (kundenummer == nr)  {             // hvis kundenummeret stemmer
+	inn.getline(buffer,STRLEN);           // leser inn navn til buffer
+	navn = new char[strlen(buffer + 1)];   // lager char til navn
 	strcpy(navn,buffer);
-	inn >> tlf;
-	inn.ignore();
+	inn >> tlf;                              // leser inn tlf nr
+	inn.ignore();                            // ignorere 1
 
-	inn.getline(buffer,STRLEN);
+	inn.getline(buffer,STRLEN);         // Leser onn gateadresse
 	gateadresse = new char[strlen(buffer + 1)];
 	strcpy(gateadresse,buffer);
 
-	inn.getline(buffer,STRLEN);
+	inn.getline(buffer,STRLEN);        // leser inn postadresse
 	postadresse = new char[strlen(buffer + 1)];
 	strcpy(postadresse,buffer);
 
-	inn.getline(buffer,STRLEN);
+	inn.getline(buffer,STRLEN);        // leser inn mail
 	mail = new char[strlen(buffer + 1)];
 	strcpy(mail,buffer);
 
-	inn >> ant;
-	inn.ignore();
-	if (ant > 0)
-		IntrSoneliste = new List(Sorted);
-	for (int i = 1; i <= ant; i++)  {
-  	intrsone = new IntrSone(inn,nr);
-	  IntrSoneliste->add(intrsone);
+	inn >> ant;                            // leser inn antall interessesoner
+	inn.ignore();                          // ignorere 1 blank
+	if (ant > 0)                           // hvis antall inrsoner > 0
+		IntrSoneliste = new List(Sorted);    // Lager ny liste 
+	for (int i = 1; i <= ant; i++)  {      // Loop gjennom antallet
+  	intrsone = new IntrSone(inn,nr);     // opprett ny intrsone
+	  IntrSoneliste->add(intrsone);        // legger intrsone til i listen
 	 }
 	}
 	else
 		cout << "FEIL! Kundenummer fra fil '"<<kundenummer<<"' stemmer ikke overens med"
 		"kundenummeret i datastrukturen: '"<<nr<<"'\n";
+}                                         // Feilmelding
 
-
-}
-
+// Destructor
 Kunde :: ~Kunde(){
-	delete [] navn; delete [] gateadresse; //Slette chararrayer
+	delete [] navn; delete [] gateadresse;       //Slette chararrayer
 	delete [] postadresse; delete [] mail;
-	delete IntrSoneliste; //Slette listen av IntrSone-objekter.
+	delete IntrSoneliste;              //Slette listen av IntrSone-objekter.
 }
 
 
-
+// Skriver Kundedata til fil
 void Kunde::skrivTilFil(ofstream & ut)  {
-	int antIntrSoner = IntrSoneliste->no_of_elements();
-	ut << kundenummer << endl;
+	int antIntrSoner = IntrSoneliste->no_of_elements(); // antall intrsoner
+	ut << kundenummer << endl;                          // Skriver ut data
 	ut << navn << endl;
 	ut << tlf << endl;
 	ut << gateadresse << endl;
@@ -157,14 +153,17 @@ void Kunde::skrivTilFil(ofstream & ut)  {
 	ut << mail << endl;
 	ut << antIntrSoner << endl;
 	ut << endl;
-	IntrSone* intrsone;
-	for (int i = 1; i <= antIntrSoner; i++)  {
-	  intrsone = (IntrSone*) IntrSoneliste->remove_no(i);
-		intrsone->skrivTilFil(ut,antIntrSoner);
-		IntrSoneliste->add(intrsone);
+	IntrSone* intrsone;                              // lager intrsone peker
+	for (int i = 1; i <= antIntrSoner; i++)  {       // loop gjennom alle intrsoner
+	  intrsone = (IntrSone*) IntrSoneliste->remove_no(i);  // fjerner fra listen
+		intrsone->skrivTilFil(ut,antIntrSoner);              // skriver til fil
+		IntrSoneliste->add(intrsone);                      // legger tilbake i listen
 	}
 }
 
+
+//Returnerer kundenummeret
 int Kunde::hentKundenummer()  {
 	return kundenummer;
 }
+
