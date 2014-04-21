98574c986151607f45e07bf966b74a9c4fcb40e9
Kristian Dragerengen <kristian@dragerengen.no>
2014-04-22 01:39:42 +0200

    Kommentering IntrSone H

--- 01 Source - Header/class IntrSone.h
+++ 01 Source - Header/class IntrSone.h
@@ -1,6 +1,7 @@
 #if !defined(__class_IntrSone_H)
 #define __class_IntrSone_H
-#include <fstream>
+
+#include <fstream>         // ifstream, ofstream
 #include "enumer.h"
 #include "listtool2.h"
 
@@ -9,31 +10,31 @@ using namespace std;
 class IntrSone : public Num_element {
 private:
 	int sonenummer, maxPris, minAreal, minAntSoverom;
-	Boligfeed boligfeeden;
-	Eiendomstype eiendomstypen;
-	Statusonske statusonsket;
+	Boligfeed boligfeeden;                   // Ukentlig, Snarest
+	Eiendomstype eiendomstypen;              //Hytte, enebolig etc
+	Statusonske statusonsket;                // Salg, Leie
 
 
 public:
-	IntrSone();
-	IntrSone(ifstream & inn, int nr);
-	IntrSone(int sonenr);
-	IntrSone(IntrSone* intrsonen, int sonenr);
-	~IntrSone();
-	void endreSonenr(int nyttnr);
-	void endreIntrSone();
-	void display();               // Displater info om IntrSone
-	void skrivTilFil(ofstream & ut, int nr);
-	int hentsonenum();
-	void statusonskeFunk();
-	char hentBoligfeeden();
-	void hentInt(int* i, const char s);
-	void boligfeedFunk();
-	void eiendomstypeFunk();
-	void enumBoligfeedSwitch(int nr);
-	void enumEiendomstypeSwitch(int nr);
-	void enumStatusSwitch(int nr);
-	const char* hentEnum(const char* s);
+	IntrSone();                     // Tom  constructor
+	IntrSone(ifstream & inn, int nr); //Constructor som leser fra fil
+	IntrSone(int sonenr);             // Constructor. Leser inn data om Intrsone
+	IntrSone(IntrSone* intrsonen, int sonenr); // Kopierer data
+	~IntrSone();                          // Destructor
+	void endreSonenr(int nyttnr);         // Endrer sonens nummer
+	void endreIntrSone();                  // Endrer data om Intrsone
+	void display();                        // Displater info om IntrSone
+	void skrivTilFil(ofstream & ut, int nr); // Skriver data til fil
+	int hentsonenum();                       // Henter sonenummeret
+	void statusonskeFunk();                  // Leser inn enum verdi
+	char hentBoligfeeden();                  // Leser inn enum verdi
+	void hentInt(int* i, const char s);      // Henter en int og sammenlikner
+	void boligfeedFunk();                    // Leser inn enum verdi
+	void eiendomstypeFunk();                 // Leser inn enum verdi                
+	void enumBoligfeedSwitch(int nr);        // Setter enum til riktig verdi
+	void enumEiendomstypeSwitch(int nr);     // Setter enum til riktig verdi
+	void enumStatusSwitch(int nr);           // Setter enum til riktig verdi
+	const char* hentEnum(const char* s);     // Return enumverdi som peker
 };
 
 #endif
